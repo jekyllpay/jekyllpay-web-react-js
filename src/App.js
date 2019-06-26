38fb4a01d908d37f13da0b2891b1ec6cec5e69a0
medmin <erichui329@gmail.com>
2019-06-25 21:28:12 -0700

    switch to material-ui

--- src/App.js
+++ src/App.js
@@ -1,92 +1,14 @@
 import React, { Component } from 'react';
-import logo from './assets/logo.png';
-import {
-  Container,
-  Divider,
-  Dropdown,
-  Grid,
-  Header,
-  Icon,
-  Image,
-  List,
-  Menu,
-  Segment,
-  Visibility,
-} from 'semantic-ui-react';
-
-const menuStyle = {
-  border: 'none',
-  borderRadius: 0,
-  boxShadow: 'none',
-  margin: 0,
-  transition: 'box-shadow 0.5s ease, padding 0.5s ease',
-}
-
-const fixedMenuStyle = {
-  backgroundColor: '#fff',
-  border: '1px solid #ddd',
-  boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.2)',
-}
+import NavBar from './components/common/NavBar'
 
 class App extends Component {
 
-  state = {
-    menuFixed: false,
-    overlayFixed: false,
-  }
-
-
-
-  render() {
-
-    const { menuFixed, overlayFixed, overlayRect } = this.state
-  
-
+  render(){
     return (
-      <div className="App">
-        <Visibility
-          onBottomPassed={this.stickTopMenu}
-          onBottomVisible={this.unStickTopMenu}
-          once={false}
-        >
-          <Menu
-            borderless
-            fixed={menuFixed ? 'top' : undefined}
-            style={menuFixed ? fixedMenuStyle : menuStyle}
-          >
-            <Container text>
-              <Menu.Item onClick={ ()=> console.log('go to homepage')}>
-                <Image size='tiny' src={logo} />
-              </Menu.Item>
-              <Menu.Item header>Jekyll Pay</Menu.Item>
-              <Menu.Item header as='a'>Pricing</Menu.Item>
-              <Menu.Item as='a'>Articles</Menu.Item>
-
-              <Menu.Menu position='right'>
-                <Dropdown text='More' pointing className='link item'>
-                  <Dropdown.Menu>
-                    <Dropdown.Item>List Item</Dropdown.Item>
-                    <Dropdown.Item>List Item</Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Header>Header Item</Dropdown.Header>
-                    <Dropdown.Item>
-                      <i className='dropdown icon' />
-                      <span className='text'>Submenu</span>
-                      <Dropdown.Menu>
-                        <Dropdown.Item>List Item</Dropdown.Item>
-                        <Dropdown.Item>List Item</Dropdown.Item>
-                      </Dropdown.Menu>
-                    </Dropdown.Item>
-                    <Dropdown.Item>List Item</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </Menu.Menu>
-            </Container>
-          </Menu>
-        </Visibility>
-      </div>
-    );
+      <NavBar />
+    )
   }
+
 }
 
 export default App;
---
+++ src/components/common/NavBar.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import logo from '../../assets/logo.png';
+import { AppBar,Toolbar, Grid, Avatar, Typography } from '@material-ui/core';
+  
+class NavBar extends Component {
+
+render() {
+    return ( 
+    <AppBar>
+        <Toolbar variant="dense">
+            <Grid container alignContent="flex-start" alignItems="center" spacing={1}>
+                <Grid item>
+                    <Avatar alt="logo" src={logo}  />
+                </Grid>
+                <Grid item>
+                    <Typography variant="subtitle1" noWrap>
+                        Jekyll Pay
+                    </Typography>
+                </Grid>
+            </Grid>
+        </Toolbar>
+    </AppBar>
+    )
+}
+
+}
+
+export default NavBar
